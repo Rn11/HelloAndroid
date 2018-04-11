d17aa6818ef974f0b3388ca05f6654fb7b6b1911
Rn11 <max.lukas-nuelle@stud.hshl.de>
2018-04-11 02:07:47 +0200

    Changed button id

--- app/src/main/java/com/example/rn11/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/rn11/helloandroid/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.rn11.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private int value = 0;
+    private Button btnClickMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnClickMe = (Button) findViewById(R.id.btnClickMe);
+        btnClickMe.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        float size;
+        switch(v.getId()){
+
+        }
     }
 }
